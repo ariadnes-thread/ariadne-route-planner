2905c988449628436417f8f72eea063f2aec265f
Matthew Wu <mewu@caltech.edu>
2018-06-05 20:01:38 -0700

    Print instead of pprint encoded route
    The encoded route is already a string, so don't need to pprint

--- planner/routers/orienteering_router.py
+++ planner/routers/orienteering_router.py
@@ -396,7 +396,7 @@ def main():
     #     {'type': 'GeometryCollection', 'geometries': linestringlist}
     # ))
 
-    pprint(RouteEncoder().encode(route))
+    print(RouteEncoder().encode(route))
 
 
 if __name__ == '__main__':
